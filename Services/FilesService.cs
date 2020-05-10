cc8420829d5b594f403a369b93473e704ee4c6d4
Welcome Sithole <16454247+SitholeWB@users.noreply.github.com>
2020-05-10 10:41:05 +0200

    Use Task.Run(()=>{}) for LiteBD non async methods

--- Services/FilesService.cs
+++ Services/FilesService.cs
@@ -31,68 +31,85 @@ namespace Services
 
 		public async Task<(Stream, FileDetails)> DownloadFileAsync(string id)
 		{
-			using (var db = new LiteDatabase(_settingsService.GetLiteDBAppSettings().ConnectionString))
+			return await Task.Run(() =>
 			{
-				var collection = db.GetCollection<FileDetails>("FileDetails");
-				var fileDetails = collection.FindById(id);
-				var stream = db.FileStorage.OpenRead(id);
-				return await Task.FromResult((stream, fileDetails));
-			}
+				using (var db = new LiteDatabase(_settingsService.GetLiteDBAppSettings().ConnectionString))
+				{
+					var collection = db.GetCollection<FileDetails>("FileDetails");
+					var fileDetails = collection.FindById(id);
+					var stream = db.FileStorage.OpenRead(id);
+					return (stream, fileDetails);
+				}
+			});
 		}
 
 		public async Task<IEnumerable<FileDetails>> GetAllFileDetailsAsync()
 		{
-			using (var db = new LiteDatabase(_settingsService.GetLiteDBAppSettings().ConnectionString))
-			{
-				var collection = db.GetCollection<FileDetails>("FileDetails");
-				return await Task.FromResult(collection.Query().ToList());
-			}
+			return await Task.Run(() =>
+		   {
+			   using (var db = new LiteDatabase(_settingsService.GetLiteDBAppSettings().ConnectionString))
+			   {
+				   var collection = db.GetCollection<FileDetails>("FileDetails");
+				   return collection.Query().ToList();
+			   }
+		   });
 		}
 
 		public async Task<FileDetails> GetFileDetailsAsync(string id)
 		{
-			using (var db = new LiteDatabase(_settingsService.GetLiteDBAppSettings().ConnectionString))
+			return await Task.Run(() =>
 			{
-				var collection = db.GetCollection<FileDetails>("FileDetails");
-				return await Task.FromResult(collection.FindById(id));
-			}
+				using (var db = new LiteDatabase(_settingsService.GetLiteDBAppSettings().ConnectionString))
+				{
+					var collection = db.GetCollection<FileDetails>("FileDetails");
+					return collection.FindById(id);
+				}
+			});
 		}
 
 		public async Task<IEnumerable<FileDetails>> GetFileDetailsByTagAsync(string tag)
 		{
-			using (var db = new LiteDatabase(_settingsService.GetLiteDBAppSettings().ConnectionString))
+			return await Task.Run(() =>
 			{
-				var collection = db.GetCollection<FileDetails>("FileDetails");
-				var result = collection.Query().Where(a => a.Tags.Contains(tag.Trim())).ToList();
-				return await Task.FromResult(result);
-			}
+				using (var db = new LiteDatabase(_settingsService.GetLiteDBAppSettings().ConnectionString))
+				{
+					var collection = db.GetCollection<FileDetails>("FileDetails");
+					return collection.Query().Where(a => a.Tags.Contains(tag.Trim())).ToList();
+				}
+			});
 		}
 
 		public async Task<FileDetails> UpdateFileDetailsAsync(FileDetails details)
 		{
-			using (var db = new LiteDatabase(_settingsService.GetLiteDBAppSettings().ConnectionString))
+			return await Task.Run(() =>
 			{
-				var collection = db.GetCollection<FileDetails>("FileDetails");
-				var fileDetails = collection.FindById(details.Id);
-				fileDetails.Description = details.Description;
-				fileDetails.LastModified = DateTime.UtcNow;
-				fileDetails.Name = details.Name;
-				fileDetails.Tags = details.Tags;
-				var success = collection.Update(fileDetails);
-				return await Task.FromResult((success ? fileDetails : throw new Exception("Error while updating")));
-			}
+				using (var db = new LiteDatabase(_settingsService.GetLiteDBAppSettings().ConnectionString))
+				{
+					var collection = db.GetCollection<FileDetails>("FileDetails");
+					var fileDetails = collection.FindById(details.Id);
+					fileDetails.Description = details.Description;
+					fileDetails.LastModified = DateTime.UtcNow;
+					fileDetails.Name = details.Name;
+					fileDetails.Tags = details.Tags;
+					var success = collection.Update(fileDetails);
+					return (success ? fileDetails : throw new Exception("Error while updating"));
+				}
+			});
 		}
 
 		public async Task<FileDetails> UploadFileAsync(Stream fileStream, FileDetails fileDetails)
 		{
-			using (var db = new LiteDatabase(_settingsService.GetLiteDBAppSettings().ConnectionString))
+			return await Task.Run(() =>
 			{
-				var collection = db.GetCollection<FileDetails>("FileDetails");
-				fileDetails.Id = ObjectId.NewObjectId().ToString();
-				collection.Insert(fileDetails.Id, fileDetails);
-				var obj = db.FileStorage.Upload(fileDetails.Id, fileDetails.Name, fileStream);
-			}
-			return await Task.FromResult(fileDetails);
+				using (var db = new LiteDatabase(_settingsService.GetLiteDBAppSettings().ConnectionString))
+				{
+					var collection = db.GetCollection<FileDetails>("FileDetails");
+					fileDetails.Id = ObjectId.NewObjectId().ToString();
+					collection.Insert(fileDetails.Id, fileDetails);
+					var obj = db.FileStorage.Upload(fileDetails.Id, fileDetails.Name, fileStream);
+					return fileDetails;
+				}
+			});
 		}
 	}
 }
