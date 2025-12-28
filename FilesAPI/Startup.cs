57013b06ab3b9a9fc98d9f650b95bea6861e14fe
Welcome Sithole <sitholewb@gmail.com>
2025-12-28 15:44:47 +0200

    Upgrade to dotnet-10

--- FilesAPI/Startup.cs
+++ FilesAPI/Startup.cs
@@ -1,4 +1,3 @@
-using System;
 using Contracts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
@@ -9,7 +8,7 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using Models.Events;
 using Models.Exceptions;
 using Models.Settings;
@@ -18,145 +17,146 @@ using Services;
 using Services.Events;
 using Services.Events.Handlers;
 using Services.Repositories;
+using System;
 
 namespace FilesAPI
 {
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-		public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			// Configure Kestrel server options for unlimited file uploads
-			services.Configure<KestrelServerOptions>(options =>
-			{
-				options.Limits.MaxRequestBodySize = null; // Remove limit completely
-				options.Limits.MinRequestBodyDataRate = null; // Remove timeout for slow uploads
-				options.Limits.MinResponseDataRate = null; // Remove timeout for slow downloads
-				options.Limits.MaxConcurrentConnections = null; // Remove connection limit
-				options.Limits.MaxConcurrentUpgradedConnections = null; // Remove upgraded connection limit
-			});
-			
-			// Note: IIS file size limits are configured via web.config
-			// See web.config for maxAllowedContentLength and requestLimits configuration
-			// Configure form options for unlimited file uploads
-			services.Configure<FormOptions>(options =>
-			{
-				options.ValueLengthLimit = int.MaxValue; // Remove form value length limit
-				options.MultipartBodyLengthLimit = long.MaxValue; // Remove multipart body limit
-				options.MultipartHeadersLengthLimit = int.MaxValue; // Remove header length limit
-				options.MultipartBoundaryLengthLimit = int.MaxValue; // Remove boundary length limit
-				options.KeyLengthLimit = int.MaxValue; // Remove key length limit
-				options.ValueCountLimit = int.MaxValue; // Remove value count limit
-				options.BufferBody = true; // Buffer the request body
-				options.MemoryBufferThreshold = int.MaxValue; // Set memory buffer threshold
-			});
-			services.AddCors(options =>
-			{
-				options.AddPolicy("AllowAll",
-					builder =>
-					{
-						builder
-						.AllowAnyOrigin()
-						.AllowAnyMethod()
-						.AllowAnyHeader();
-					});
-			});
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new OpenApiInfo
-				{
-					Title = "Files API",
-					Version = "v1"
-				});
-			});
-			//App Settings Injection
-			services.Configure<MongoDBAppSettings>(Configuration.GetSection("MongoDBAppSettings"));
-			services.Configure<LiteDBAppSettings>(Configuration.GetSection("LiteDBAppSettings"));
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configure Kestrel server options for unlimited file uploads
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.Limits.MaxRequestBodySize = null; // Remove limit completely
+                options.Limits.MinRequestBodyDataRate = null; // Remove timeout for slow uploads
+                options.Limits.MinResponseDataRate = null; // Remove timeout for slow downloads
+                options.Limits.MaxConcurrentConnections = null; // Remove connection limit
+                options.Limits.MaxConcurrentUpgradedConnections = null; // Remove upgraded connection limit
+            });
 
-			// Configure database services based on environment
-			var useEmbeddedDatabase = Configuration.GetValue<bool>("USE_EMBEDDED_DATABASE", false) || 
-			                         Environment.GetEnvironmentVariable("USE_EMBEDDED_DATABASE") == "true";
-			
-			if (useEmbeddedDatabase)
-			{
-				// Use LiteDB for self-contained operation
-				var databasePath = Environment.GetEnvironmentVariable("DATABASE_PATH") ?? 
-				                  Configuration.GetValue<string>("DATABASE_PATH", "./data/filesapi.db");
-				var uploadsPath = Environment.GetEnvironmentVariable("UPLOADS_PATH") ?? 
-				                 Configuration.GetValue<string>("UPLOADS_PATH", "./uploads");
-				
-				services.AddScoped<IStorageRepository>(provider => 
-					new Services.Repositories.LiteDbStorageRepository(databasePath, uploadsPath));
-				services.AddScoped<IFileDetailsRepository>(provider => 
-					new Services.Repositories.LiteDbFileDetailsRepository(databasePath));
-				services.AddScoped<IDownloadAnalyticsRepository>(provider => 
-					new Services.Repositories.LiteDbDownloadAnalyticsRepository(databasePath));
-			}
-			else
-			{
-				// Use MongoDB for traditional operation
-				services.AddScoped<IStorageRepository, StorageRepository>();
-				services.AddScoped<IFileDetailsRepository, FileDetailsRepository>();
-				services.AddScoped<IDownloadAnalyticsRepository, Services.Repositories.MongoDbDownloadAnalyticsRepository>();
-			}
+            // Note: IIS file size limits are configured via web.config See web.config for
+            // maxAllowedContentLength and requestLimits configuration Configure form options for
+            // unlimited file uploads
+            services.Configure<FormOptions>(options =>
+            {
+                options.ValueLengthLimit = int.MaxValue; // Remove form value length limit
+                options.MultipartBodyLengthLimit = long.MaxValue; // Remove multipart body limit
+                options.MultipartHeadersLengthLimit = int.MaxValue; // Remove header length limit
+                options.MultipartBoundaryLengthLimit = int.MaxValue; // Remove boundary length limit
+                options.KeyLengthLimit = int.MaxValue; // Remove key length limit
+                options.ValueCountLimit = int.MaxValue; // Remove value count limit
+                options.BufferBody = true; // Buffer the request body
+                options.MemoryBufferThreshold = int.MaxValue; // Set memory buffer threshold
+            });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                    });
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Files API",
+                    Version = "v1"
+                });
+            });
+            //App Settings Injection
+            services.Configure<MongoDBAppSettings>(Configuration.GetSection("MongoDBAppSettings"));
+            services.Configure<LiteDBAppSettings>(Configuration.GetSection("LiteDBAppSettings"));
 
-			//services.AddSingleton<IStorageService, FilesService>();
-			services.AddScoped<IStorageService, StorageService>();
-			services.AddScoped<IAnalyticsService, AnalyticsService>();
-			services.AddSingleton<ISettingsService, SettingsService>();
+            // Configure database services based on environment
+            var useEmbeddedDatabase = Configuration.GetValue<bool>("USE_EMBEDDED_DATABASE", false) ||
+                                     Environment.GetEnvironmentVariable("USE_EMBEDDED_DATABASE") == "true";
 
-			services.AddScoped<RecordDownloadHandler>();
-			services.AddScoped<EventHandlerContainer>();
-			EventHandlerContainer.Subscribe<FileDownloadedEvent, RecordDownloadHandler>();
+            if (useEmbeddedDatabase)
+            {
+                // Use LiteDB for self-contained operation
+                var databasePath = Environment.GetEnvironmentVariable("DATABASE_PATH") ??
+                                  Configuration.GetValue<string>("DATABASE_PATH", "./data/filesapi.db");
+                var uploadsPath = Environment.GetEnvironmentVariable("UPLOADS_PATH") ??
+                                 Configuration.GetValue<string>("UPLOADS_PATH", "./uploads");
 
-			services.AddControllers();
-		}
+                services.AddScoped<IStorageRepository>(provider =>
+                    new Services.Repositories.LiteDbStorageRepository(databasePath, uploadsPath));
+                services.AddScoped<IFileDetailsRepository>(provider =>
+                    new Services.Repositories.LiteDbFileDetailsRepository(databasePath));
+                services.AddScoped<IDownloadAnalyticsRepository>(provider =>
+                    new Services.Repositories.LiteDbDownloadAnalyticsRepository(databasePath));
+            }
+            else
+            {
+                // Use MongoDB for traditional operation
+                services.AddScoped<IStorageRepository, StorageRepository>();
+                services.AddScoped<IFileDetailsRepository, FileDetailsRepository>();
+                services.AddScoped<IDownloadAnalyticsRepository, Services.Repositories.MongoDbDownloadAnalyticsRepository>();
+            }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			app.UseCors("AllowAll");
-			app.UseStaticFiles();
-			var swaggerUrl = "/swagger/v1/swagger.json";
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+            //services.AddSingleton<IStorageService, FilesService>();
+            services.AddScoped<IStorageService, StorageService>();
+            services.AddScoped<IAnalyticsService, AnalyticsService>();
+            services.AddSingleton<ISettingsService, SettingsService>();
 
-			app.UseSwagger();
-			app.UseSwaggerUI(c =>
-			{
-				c.SwaggerEndpoint(swaggerUrl, "Files API V1");
-				c.RoutePrefix = string.Empty;
-			});
+            services.AddScoped<RecordDownloadHandler>();
+            services.AddScoped<EventHandlerContainer>();
+            EventHandlerContainer.Subscribe<FileDownloadedEvent, RecordDownloadHandler>();
 
-			app.UseHttpsRedirection();
-			app.UseRouting();
+            services.AddControllers();
+        }
 
-			app.UseExceptionHandler(a => a.Run(async context =>
-			{
-				var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
-				var exception = exceptionHandlerPathFeature.Error;
-				if (exception is FilesApiException)
-				{
-					var result = JsonConvert.SerializeObject(new { error = exception.Message });
-					context.Response.ContentType = "application/json";
-					context.Response.StatusCode = StatusCodes.Status400BadRequest;
-					await context.Response.WriteAsync(result);
-				}
-			}));
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors("AllowAll");
+            app.UseStaticFiles();
+            var swaggerUrl = "/swagger/v1/swagger.json";
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllers();
-			});
-		}
-	}
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(swaggerUrl, "Files API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseExceptionHandler(a => a.Run(async context =>
+            {
+                var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                var exception = exceptionHandlerPathFeature.Error;
+                if (exception is FilesApiException)
+                {
+                    var result = JsonConvert.SerializeObject(new { error = exception.Message });
+                    context.Response.ContentType = "application/json";
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    await context.Response.WriteAsync(result);
+                }
+            }));
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
 }
--- Services/FilesService.cs
+++ Services/FilesService.cs
@@ -9,41 +9,41 @@ using System.Threading.Tasks;
 
 namespace Services
 {
-	public sealed class FilesService //: IStorageService
-	{
-		private readonly ILiteDatabase _liteDatabase;
+    public sealed class FilesService //: IStorageService
+    {
+        private readonly ILiteDatabase _liteDatabase;
 
-		public FilesService(ISettingsService settingsService)
-		{
-			_liteDatabase = new LiteDatabase(settingsService.GetLiteDBAppSettings().ConnectionString);
-		}
+        public FilesService(ISettingsService settingsService)
+        {
+            _liteDatabase = new LiteDatabase(settingsService.GetLiteDBAppSettings().ConnectionString);
+        }
 
-		public void Dispose()
-		{
-			_liteDatabase.Dispose();
-			GC.SuppressFinalize(this);
-		}
+        public void Dispose()
+        {
+            _liteDatabase.Dispose();
+            GC.SuppressFinalize(this);
+        }
 
-		public async Task<string> DeleteFileAsync(string id)
-		{
-			return await Task.Run(() =>
-			{
-				var collection = _liteDatabase.GetCollection<FileDetails>("FileDetails");
-				var success = collection.Delete(id);
-				return (success ? id : string.Empty);
-			});
-		}
+        public async Task<string> DeleteFileAsync(string id)
+        {
+            return await Task.Run(() =>
+            {
+                var collection = _liteDatabase.GetCollection<FileDetails>("FileDetails");
+                var success = collection.Delete(id);
+                return (success ? id : string.Empty);
+            });
+        }
 
-		public async Task<(Stream, FileDetails)> DownloadFileAsync(string id)
-		{
-			return await Task.Run(() =>
-			{
-				var collection = _liteDatabase.GetCollection<FileDetails>("FileDetails");
-				var fileDetails = collection.FindById(id);
-				var stream = _liteDatabase.FileStorage.OpenRead(id);
-				return (stream, fileDetails);
-			});
-		}
+        public async Task<(Stream, FileDetails)> DownloadFileAsync(string id)
+        {
+            return await Task.Run(() =>
+            {
+                var collection = _liteDatabase.GetCollection<FileDetails>("FileDetails");
+                var fileDetails = collection.FindById(id);
+                var stream = _liteDatabase.FileStorage.OpenRead(id);
+                return (stream, fileDetails);
+            });
+        }
 
         public async Task IncrementDownloadCountAsync(string id)
         {
@@ -54,7 +54,7 @@ namespace Services
                 if (fileDetails != null)
                 {
                     // Increment the download count
-                    fileDetails.DownloadCount++;
+                    fileDetails.NumberOfDownloads++;
                     // Update the file details in the collection
                     var success = collection.Update(fileDetails);
                     if (!success)
@@ -70,46 +70,46 @@ namespace Services
         }
 
         public async Task<IEnumerable<FileDetails>> GetAllFileDetailsAsync()
-		{
-			return await Task.Run(() =>
-		   {
-			   var collection = _liteDatabase.GetCollection<FileDetails>("FileDetails");
-			   return collection.Query().ToList();
-		   });
-		}
+        {
+            return await Task.Run(() =>
+           {
+               var collection = _liteDatabase.GetCollection<FileDetails>("FileDetails");
+               return collection.Query().ToList();
+           });
+        }
 
-		public async Task<FileDetails> GetFileDetailsAsync(string id)
-		{
-			return await Task.Run(() =>
-			{
-				var collection = _liteDatabase.GetCollection<FileDetails>("FileDetails");
-				return collection.FindById(id);
-			});
-		}
+        public async Task<FileDetails> GetFileDetailsAsync(string id)
+        {
+            return await Task.Run(() =>
+            {
+                var collection = _liteDatabase.GetCollection<FileDetails>("FileDetails");
+                return collection.FindById(id);
+            });
+        }
 
-		public async Task<IEnumerable<FileDetails>> GetFileDetailsByTagAsync(string tag)
-		{
-			return await Task.Run(() =>
-			{
-				var collection = _liteDatabase.GetCollection<FileDetails>("FileDetails");
-				return collection.Query().Where(a => a.Tags.Contains(tag.Trim())).ToList();
-			});
-		}
+        public async Task<IEnumerable<FileDetails>> GetFileDetailsByTagAsync(string tag)
+        {
+            return await Task.Run(() =>
+            {
+                var collection = _liteDatabase.GetCollection<FileDetails>("FileDetails");
+                return collection.Query().Where(a => a.Tags.Contains(tag.Trim())).ToList();
+            });
+        }
 
-		public async Task<FileDetails> UpdateFileDetailsAsync(FileDetails details)
-		{
-			return await Task.Run(() =>
-			{
-				var collection = _liteDatabase.GetCollection<FileDetails>("FileDetails");
-				var fileDetails = collection.FindById(details.Id);
-				fileDetails.Description = details.Description;
-				fileDetails.LastModified = DateTime.UtcNow;
-				fileDetails.Name = details.Name;
-				fileDetails.Tags = details.Tags;
-				var success = collection.Update(fileDetails);
-				return (success ? fileDetails : throw new Exception("Error while updating"));
-			});
-		}
+        public async Task<FileDetails> UpdateFileDetailsAsync(FileDetails details)
+        {
+            return await Task.Run(() =>
+            {
+                var collection = _liteDatabase.GetCollection<FileDetails>("FileDetails");
+                var fileDetails = collection.FindById(details.Id);
+                fileDetails.Description = details.Description;
+                fileDetails.LastModified = DateTime.UtcNow;
+                fileDetails.Name = details.Name;
+                fileDetails.Tags = details.Tags;
+                var success = collection.Update(fileDetails);
+                return (success ? fileDetails : throw new Exception("Error while updating"));
+            });
+        }
 
         public async Task<FileDetails> UploadFileAsync(Stream fileStream, FileDetails fileDetails)
         {
